3a069cd1e6f6acdbc1cb87804fc030a0cda4b24c
Jamie Bayne <jamiebayne@hotmail.com>
2018-05-11 16:02:12 +0100

    Adjust first FC layer for channel transposition

--- caffe2keras.py
+++ caffe2keras.py
@@ -6,7 +6,7 @@ CAFFE_WEIGHTS_FILE = "conv3d_deepnetA_sport1m_iter_1900000"
 CONV_LAYERS = [1,4,7,9,12,14,17,19]
 FC_LAYERS = [22,25,28]
 OUTPUT = "keras_weights.pkl.xz"
-FLIP_3D = False
+FLIP_3D = True
 
 import caffe_pb2 as pb
 import numpy as np
@@ -41,6 +41,12 @@ def numpy_fc_weights(w):
     w = w.T
     return w
 
+def first_fc_layer(w):
+    # layer is 8192 -> 4096, with 8192 from channels first format 512,1,4,4
+    # need channels last format 1,4,4,512
+    assert w.shape == (8192,4096)
+    return np.reshape(w,(512,4,4,4096)).transpose((1,2,0,3)).reshape((8192,4096))
+
 def numpy_param(p):
     shape = get_param_shape(p)
     p = np.array(p.data).reshape(shape)
@@ -51,6 +57,9 @@ print("Converting...")
 for l in CONV_LAYERS + FC_LAYERS:
     w,b = caffe_params.layers[l].blobs
     w = numpy_fc_weights(w) if l in FC_LAYERS else numpy_conv_weights(w)
+    if l == FC_LAYERS[0]:
+        w = first_fc_layer(w)
+
     print(w.shape)
     b = numpy_param(b)
     print(b.shape)
